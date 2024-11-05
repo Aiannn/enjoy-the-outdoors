d1730dded190092bdcc790f5c9928ff5dffd4fa5
Aiannn <soktoevayan@gmail.com>
2024-11-05 20:32:50 +0300

    styling and minor changes left

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -6,6 +6,8 @@ function loadMountains(params) {
     let option = new Option(mountain.name, mountain.elevation)
     option.dataset.description = mountain.desc
     option.dataset.image = mountain.img
+    option.dataset.latitude = mountain.coords.lat
+    option.dataset.longitude = mountain.coords.lng
     console.log(option)
     select.append(option)
   }
@@ -28,6 +30,39 @@ function showMountain() {
   cardImg.src = './images/' + selectedOption.dataset.image
   console.log(typeof selectedOption.dataset.image)
 
+
+  let cardSunrise = document.querySelector('#card-mountain-sunrise')
+  let cardSunset = document.querySelector('#card-mountain-sunset')
+  let sunResults = fetchSunsetTimes(selectedOption.dataset.latitude, selectedOption.dataset.longitude)
+  
+  // cardSunrise.innerText = sunResults.results
+  // cardSunset.innerText = sunResults.results
+
+
+
   let div = document.querySelector('#card-mountain')
   div.style.display = 'block'
 }
+
+async function fetchSunsetTimes(lat, lng) {
+  let cardSunrise = document.querySelector('#card-mountain-sunrise')
+  let cardSunset = document.querySelector('#card-mountain-sunset')
+  let results = null
+  fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    return response.json()
+  })
+  .then(data => {
+    results = data
+    console.log(data)
+    cardSunrise.innerText = data.results.sunrise
+    cardSunset.innerText = data.results.sunset
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error)
+  })
+  return results
+} 
