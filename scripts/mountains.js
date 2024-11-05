be28b5337be6835eacb6e30fee1bcb267006926b
Aiannn <soktoevayan@gmail.com>
2024-11-05 17:39:15 +0300

    js done

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,7 +1,33 @@
 "use strict";
 
 function loadMountains(params) {
+  let select = document.querySelector('#select-mountain')
   for (const mountain of mountainsArray) {
-    console.log(mountain);
+    let option = new Option(mountain.name, mountain.elevation)
+    option.dataset.description = mountain.desc
+    option.dataset.image = mountain.img
+    console.log(option)
+    select.append(option)
   }
+}
+loadMountains()
+
+function showMountain() {
+  let select = document.querySelector('#select-mountain')
+  let selectedOption = select.options[select.selectedIndex]
+  console.log(selectedOption)
+
+  let cardName = document.querySelector('#card-mountain-title')
+  cardName.textContent = selectedOption.innerText
+  console.log(selectedOption.name, selectedOption)
+  let cardDesc = document.querySelector('#card-mountain-desc')
+  cardDesc.textContent = selectedOption.dataset.description
+  let cardElev = document.querySelector('#card-mountain-elevation')
+  cardElev.textContent = selectedOption.value + ' feet'
+  let cardImg = document.querySelector('#card-mountain-image')
+  cardImg.src = './images/' + selectedOption.dataset.image
+  console.log(typeof selectedOption.dataset.image)
+
+  let div = document.querySelector('#card-mountain')
+  div.style.display = 'block'
 }
--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -1,6 +1,71 @@
+function loadParks(parks) {
 
-function loadParks() {
-  for (const park of nationalParksArray) {
+  const columns = [
+    "LocationID", "LocationName", "Address", "City", "State", "ZipCode",
+    "Phone", "Fax", "Visit", "Latitude", "Longitude", "Location"
+  ];
+
+  let tableBody = document.querySelector('#table-body')
+  for (const park of parks) {
+    let tr = document.createElement('tr')
+    tableBody.append(tr) 
+
+    // use the predefined array on line 3
+    for (column of columns) {
+      let td = document.createElement('td')
+      if (column === 'Visit') {
+        td.innerText = park[column] || 'N/A'
+      } else {
+        td.innerText = park[column]
+      }
+      tr.append(td)
+    }
   }
+}
+loadParks(nationalParksArray)
+
+function loadOptions(statesOrTypes) {
+  let select = document.querySelector('#select')
+  for (let element of statesOrTypes) {
+    let option = new Option(element, element) 
+    select.append(option)
+  }
+}
+
+function showSelect() {
+  let select = document.querySelector('#select')
+  let radioByState = document.querySelector('#radio-by-state')
+  let radioByType = document.querySelector('#radio-by-type')
+
+  select.innerHTML = ''
+  if (radioByState.checked) {
+    select.style.display = 'block'
+    loadOptions(locationsArray)
+  } else if (radioByType.checked) {
+    select.style.display = 'block'
+    loadOptions(parkTypesArray)
+  } else {
+    null
+  }
+}
+
+function filterBy() {
+  let radioByState = document.querySelector('#radio-by-state')
+  let radioByType = document.querySelector('#radio-by-type')
+
+  let tableBody = document.querySelector('#table-body')
+  tableBody.innerHTML = ''
+
+  let select = document.querySelector('select')
+  let newArr = nationalParksArray.filter(park => {
+    if (radioByState.checked) {
+      return select.value === park.State
+    } else if (radioByType.checked) {
+      return park.LocationName.includes(select.value)
+    }
+  })
+  console.log(newArr)
+  loadParks(newArr)
+
 }
 
